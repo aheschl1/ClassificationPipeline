4f1700afb616c0a7d8afc61c63c16a7a59f6123f
aheschl1 <ajheschl@gmail.com>
2023-08-28 14:26:12 -0600

    starting on basic needs

--- src/dataloading/datapoint.py
+++ src/dataloading/datapoint.py
@@ -1,12 +1,15 @@
 import numpy as np
 from ..utils.constants import *
+from ..utils.readers import get_reader
 
 
 class Datapoint:
-    def __init__(self, path: str, label: int) -> None:
+    def __init__(self, path: str, label: int, reader: str = SIMPLE_ITK) -> None:
         self.path = path
         self.label = label
+        self._reader = get_reader(reader)()
 
-    def get_data(self, reader: str = SIMPLE_ITK) -> np.array:
-        assert reader in [SIMPLE_ITK], 'Unsupported reader specified'
+    def get_data(self) -> np.array:
+        return self._reader.read(self.path)
+
 
