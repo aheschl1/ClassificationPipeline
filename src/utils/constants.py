7ac9068ebe7f43a4a47d80b13769eacdc6758bf1
aheschl1 <ajheschl@gmail.com>
2023-09-03 14:10:53 -0600

    womp womp

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -5,8 +5,8 @@ NATURAL = 'NATURAL'
 CT = 'CT'
 DATA_ROOT = os.getenv('DATASET_ROOT')
 if DATA_ROOT is None or not os.path.exists(DATA_ROOT):
-    raise NotADirectoryError('You must define $DATASET_ROOT in your environment variables (in .bashrc), and make sure '
-                             'that the path exists.')
+    raise NotADirectoryError('You must define $DATASET_ROOT in your environment variables '
+                             '(in ~/.bashrc or ~/.profile), and make sure that the path exists.')
 
 RAW_ROOT = f"{DATA_ROOT}/raw"
 PREPROCESSED_ROOT = f"{DATA_ROOT}/preprocessed"
